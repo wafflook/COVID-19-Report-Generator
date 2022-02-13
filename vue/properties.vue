dd1fc280bae159aad3c4f6a7d5cadb13ab50342d
Yusuke Sano <wafflook@gmail.com>
2022-02-14 08:06:06 +0900

    oops

---
+++ vue/properties.vue
@@ -0,0 +1,45 @@
+ <template>
+   <div>
+     <table class='block table ui fixed celled single line unstackable very basic'>
+       <thead>
+         <tr>
+           <th>no</th>
+           <th>name</th>
+           <th>value</th>
+        </tr>
+      </thead>
+       <tbody>
+         <tr v-for='result,name,n in debug'>
+           <td>{{n+1}}</td>
+           <td>{{name}}</td>
+           <td>{{result}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+ <script>
+   module.exports={
+     computed:{
+       debug:function(){
+         return flattie(this.value)
+       }
+     },
+     props:{
+       value:{
+         type:Object,
+         default:{}
+       }
+     }
+   }
+</script>
+ <style>
+   .properties .ui.table>thead>tr>th:nth-child(1),
+   .properties .ui.table>tbody>tr>td:nth-child(1){
+     width:27px;
+     text-align:center;
+   }
+   .properties .ui.table>tbody>tr>td:nth-child(3){
+     word-break:break-all;
+   }
+</style>
